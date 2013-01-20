ced0b9e81480a88a7dca7d5e24196f1e6ff73653
shinsuke sasaki <sasapurin@sasapurin.com>
2013-01-20 16:39:36 +0900

    HTMLソースをawkで処理する事前処理を追加
    ファイル整理の為ディレクトリ構成の変更もおこなった。

---
+++ make_source_for_awk.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+#
+# 指定URLのHTMLファイルをソースをwgetで取得してsource.txtに保存します。
+# このスクリプトの第一引数にURLを指定すると下記$1に代入されてwgetが動作します。
+wget -O source.txt $1
+#
+# trで改行コードを全て削除します。
+tr -d '\n' < source.txt > mod1.txt
+#
+# sedで改行コードをタグの頭に付加します。
+sed 's/</\n</g' mod1.txt > mod2.txt
